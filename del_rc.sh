2faf528e7fbce8244cc2af8a40da89f10071186b
arunprasath2503 <62014640+arunprasath2503@users.noreply.github.com>
2022-06-18 10:28:26 +0530

    Update del_rc.sh

--- del_rc.sh
+++ del_rc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl delete rc arun-rc -n arun
-if [$? -ne 0];then
+echo "kubectl delete rc arun-rc -n arun"
+if [$? -ne 0]; then
 echo "kubectl apply -f rs_final.yaml" 
 else 
 echo "kubectl apply -f rs_final.yaml" 
